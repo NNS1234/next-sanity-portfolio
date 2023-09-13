fd466f0f76bbe04a28125343ae913cc0f82254bd
NNS1234 <nudratsaber@gmail.com>
2023-09-13 03:45:17 -0500

    modify-1

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,16 +1,24 @@
 // components/Skills.tsx
-
 // components/SkillsSection.tsx
-
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+
 const Skills: React.FC = () => {
   const skills = [
-    'Java', 'JavaScript','React', 'Next', 'TypeScript',
-    'Tailwind',
-    'Node' ,'MySQL','PostgreSQL','AWS','Jenkins','Docker'
-    
+    'Java', 'JavaScript', 'React', 'Next', 'TypeScript',
+    'Tailwind', 'Node', 'MySQL', 'PostgreSQL', 'AWS', 'Jenkins', 'Docker'
   ];
+
+  const [currentSkill, setCurrentSkill] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSkill((prevSkill) => (prevSkill + 1) % skills.length);
+    }, 1400); // Adjust the interval duration as needed (in milliseconds)
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <section className="mt-12 py-8">
@@ -18,9 +26,17 @@ const Skills: React.FC = () => {
         <h2 className="mt-2 mb-14 font-bold text-gray-700 text-3xl">Skills & Tools</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
           {skills.map((skill, index) => (
-            <div key={index} className="text-center">
-              <Image src={`/icons/${skill}.svg`} alt={skill} width={96} // Set the width
-              height={96} className="animate-bounce w-20 h-20 mx-auto mb-2" />
+            <div
+              key={index}
+              className={`text-center ${currentSkill === index ? 'animate-bounce' : ''}`}
+            >
+              <Image
+                src={`/icons/${skill}.svg`}
+                alt={skill}
+                width={96}
+                height={96}
+                className="w-20 h-20 mx-auto mb-2"
+              />
               <p className="text-lg font-semibold">{skill}</p>
             </div>
           ))}
@@ -31,3 +47,4 @@ const Skills: React.FC = () => {
 };
 
 export default Skills;
+
